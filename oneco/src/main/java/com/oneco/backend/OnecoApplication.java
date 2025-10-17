b5617ab3e4e04ea0f4efa1754a8805e1bef57448
goodjunseon <pzs2001926@gmail.com>
2025-10-17 22:16:46 +0900

    git init

---
+++ oneco/src/main/java/com/oneco/backend/OnecoApplication.java
@@ -0,0 +1,13 @@
+package com.oneco.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnecoApplication.class, args);
+	}
+
+}
