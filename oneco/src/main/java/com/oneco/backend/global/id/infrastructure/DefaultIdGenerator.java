3dd551289563b442f446586727f461201b1f9d4b
goodjunseon <pzs2001926@gmail.com>
2025-11-11 15:24:50 +0900

    fix: DefaultIdGenerator에서 @Componenet 제거하여 IdGenerator 빈 중복 방지(#10) - DefaultGenerator에 @Component가 있어서 IdGenerator 타입의 빈이 중복 등록되는 문제가 있었습니다. IdGeneratorConfig의 @ConditionalOnMissingBean 로직이 정상 동작하도록 DefaultGenerator에서 컴포넌트 어노테이션을 제거했습니다.

--- oneco/src/main/java/com/oneco/backend/global/id/infrastructure/DefaultIdGenerator.java
+++ oneco/src/main/java/com/oneco/backend/global/id/infrastructure/DefaultIdGenerator.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 import com.oneco.backend.global.id.domain.IdGenerator;
 
-@Component
 public class DefaultIdGenerator implements IdGenerator {
 
 	@Override
