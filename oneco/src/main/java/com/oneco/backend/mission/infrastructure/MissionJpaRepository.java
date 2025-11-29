3f8bfebcfdd97beb2709f652d14b73fbb148d05a
goodjunseon <pzs2001926@gmail.com>
2025-11-29 16:13:46 +0900

    feat: Mission Infra 로직 작성(#28)

---
+++ oneco/src/main/java/com/oneco/backend/mission/infrastructure/MissionJpaRepository.java
@@ -0,0 +1,9 @@
+package com.oneco.backend.mission.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.oneco.backend.mission.domain.Mission;
+
+public interface MissionJpaRepository extends JpaRepository<Mission, Long> {
+
+}
