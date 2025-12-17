56439d36871d4cce0fdff99f1e49613240178060
goodjunseon <pzs2001926@gmail.com>
2025-12-17 15:25:38 +0900

    feat: MemberID(VO) 추가 - 외부 도메인 참조용

---
+++ oneco/src/main/java/com/oneco/backend/member/domain/MemberId.java
@@ -0,0 +1,33 @@
+package com.oneco.backend.member.domain;
+
+import com.oneco.backend.global.exception.BaseException;
+import com.oneco.backend.global.exception.constant.MemberErrorCode;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberId {
+
+	@Column(name = "member_id", nullable = false)
+	private Long value;
+
+	private MemberId(Long value) {
+		if (value == null || value <= 0) {
+			throw BaseException.from(MemberErrorCode.INVALID_MEMBER_ID);
+		}
+		this.value = value;
+	}
+
+	public static MemberId of(Long value) {
+		return new MemberId(value);
+	}
+}
+
