362b05876a13e03f0848a451b65991c88159378e
goodjunseon <pzs2001926@gmail.com>
2025-12-21 01:53:24 +0900

    remove: Repository 인터페이스 제거, JPA 구현(infrastructure)

--- oneco/src/main/java/com/oneco/backend/family/domain/relation/FamilyRelationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.oneco.backend.family.domain.relation;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FamilyRelationRepository extends JpaRepository<FamilyRelation, FamilyRelationId> {
-}
