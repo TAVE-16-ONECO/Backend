6f48896868d6bbd5ebc73f2d0cbbaafb961ca8d4
goodjunseon <pzs2001926@gmail.com>
2025-12-24 13:33:13 +0900

    remove: 값참조 방식으로 변경

--- oneco/src/main/java/com/oneco/backend/mission/application/port/out/FamilyRelationReader.java
+++
@@ -1,5 +0,0 @@
-package com.oneco.backend.mission.application.port.out;
-
-public interface FamilyRelationReader {
-	FamilyRelationInfo getById(Long familyRelationId);
-}
--- oneco/src/main/java/com/oneco/backend/mission/application/port/out/MemberInfo.java
+++
@@ -1,7 +0,0 @@
-package com.oneco.backend.mission.application.port.out;
-
-public record MemberInfo (
-	Long id,
-	String accountType, // ex) PARENT, CHILD
-	String systemRole // USER, ADMIN
-) {}
