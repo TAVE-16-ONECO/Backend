d6e884aae58da48d13bbd4ae63f58ef3219afeac
goodjunseon <pzs2001926@gmail.com>
2025-12-24 13:34:10 +0900

    feat: 카테고리, 요청자, 수신자 id 추가

--- oneco/src/main/java/com/oneco/backend/mission/domain/mission/Mission.java
+++ oneco/src/main/java/com/oneco/backend/mission/domain/mission/Mission.java
@@ -2,9 +2,11 @@ package com.oneco.backend.mission.domain.mission;
 
 import static lombok.AccessLevel.*;
 
+import com.oneco.backend.content.domain.dailycontent.CategoryId;
 import com.oneco.backend.family.domain.relation.FamilyRelationId;
 import com.oneco.backend.global.entity.BaseTimeEntity;
 import com.oneco.backend.global.exception.BaseException;
+import com.oneco.backend.member.domain.MemberId;
 import com.oneco.backend.mission.domain.exception.MissionErrorCode;
 
 import jakarta.persistence.AttributeOverride;
@@ -25,13 +27,25 @@ import lombok.NoArgsConstructor;
 public class Mission extends BaseTimeEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Embedded
+	@AttributeOverride(name = "value", column = @Column(name = "requester_id", nullable = false))
+	private MemberId requesterId; // 미션을 생성한 멤버 ID
+
+	@Embedded
+	@AttributeOverride(name = "value", column = @Column(name = "recipient_id", nullable = false))
+	private MemberId recipientId; // 요청 받는 사람 멤버 ID
 
 	// 가족 관계 ID (외래 키)
 	@Embedded
 	@AttributeOverride(name = "value", column = @Column(name = "family_relation_id", nullable = false))
 	private FamilyRelationId familyRelationId;
+
+	@Embedded
+	@AttributeOverride(name = "value", column = @Column(name = "category_id", nullable = false))
+	private CategoryId categoryId;
 
 	@Embedded
 	@Column(nullable = false)
@@ -43,26 +57,63 @@ public class Mission extends BaseTimeEntity {
 	@Enumerated(EnumType.STRING)
 	private MissionStatus status; // default: APPROVAL_REQUEST
 
-	private Mission(FamilyRelationId familyRelationId, MissionPeriod period, Reward reward) {
+	private Mission(
+		MemberId requesterId,
+		MemberId recipientId,
+		FamilyRelationId familyRelationId,
+		CategoryId categoryId,
+		MissionPeriod period,
+		Reward reward
+	) {
+
+		// requesterId null인 경우 예외 처리
+		if (requesterId == null) {
+			throw BaseException.from(
+				MissionErrorCode.MEMBER_ID_CANNOT_BE_NULL,
+				"요청자 ID는 null일 수 없습니다."
+			);
+		}
+
+		if (recipientId == null) {
+			throw BaseException.from(
+				MissionErrorCode.MEMBER_ID_CANNOT_BE_NULL,
+				"수신자 ID는 null일 수 없습니다."
+			);
+		}
 
 		// familyRelationId가 null인 경우 예외 처리
 		if (familyRelationId == null) {
 			throw BaseException.from(MissionErrorCode.FAMILY_RELATION_ID_CANNOT_BE_NULL);
 		}
 
+		// categoryId가 null인 경우 예외 처리
+		if (categoryId == null) {
+			throw BaseException.from(MissionErrorCode.CATEGORY_ID_CANNOT_BE_NULL);
+		}
+
 		if (period == null) {
 			throw BaseException.from(MissionErrorCode.MISSION_PERIOD_CANNOT_BE_NULL);
 		}
 
+		this.requesterId = requesterId;
+		this.recipientId = recipientId;
 		this.familyRelationId = familyRelationId;
+		this.categoryId = categoryId;
 		this.period = period;
 		this.reward = reward;
 		this.status = MissionStatus.APPROVAL_REQUEST; // 미션 생성 시 기본 상태 값: 승인 요청
 	}
 
 	// 미션 생성 메서드
-	public static Mission create(FamilyRelationId familyRelationId, MissionPeriod period, Reward reward) {
-		return new Mission(familyRelationId, period, reward);
+	public static Mission create(
+		MemberId requesterId,
+		MemberId recipientId,
+		FamilyRelationId familyRelationId,
+		CategoryId categoryId,
+		MissionPeriod period,
+		Reward reward
+	) {
+		return new Mission(requesterId, recipientId, familyRelationId, categoryId, period, reward);
 	}
 
 	// === 미션 상태 변경 메서드 ===
