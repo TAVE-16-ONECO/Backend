d24eb07457944cac0dd0dfc8ed08166066ed02d7
goodjunseon <pzs2001926@gmail.com>
2026-01-17 16:45:57 +0900

    feat: 단건 미션 상세 조회 API

--- oneco/src/main/java/com/oneco/backend/mission/application/port/out/CategoryLookupPort.java
+++ oneco/src/main/java/com/oneco/backend/mission/application/port/out/CategoryLookupPort.java
@@ -2,11 +2,14 @@ package com.oneco.backend.mission.application.port.out;
 
 import com.oneco.backend.category.domain.category.CategoryId;
 import com.oneco.backend.category.domain.category.MissionDays;
+import com.oneco.backend.category.domain.category.CategoryTitle;
 
 
 // 카테고리 도메인 조회 포트
 public interface CategoryLookupPort {
 
 	MissionDays getDefaultMissionDays(CategoryId categoryId);
+
+	CategoryTitle getCategoryTitle(CategoryId categoryId);
 
 }
